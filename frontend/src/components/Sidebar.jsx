b9d2f065d447389c5fa335c2797f2047e7771d68
Sridhar. <135013637+sridhar281@users.noreply.github.com>
2025-12-05 18:27:42 +0530

    Fix import case for useAuthStore in Sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import useChatStore  from "../store/useChatStore";
-import { useAuthStore } from "../store/UseAuthStore";
+import { useAuthStore } from "../store/useAuthStore";
 import SidebarSkeleton from "./skeletons/SidebarSkeleton";
 import { Users } from "lucide-react";
 
