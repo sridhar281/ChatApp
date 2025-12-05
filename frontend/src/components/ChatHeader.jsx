161ae65711b251589650819c16f4084b0f43a2d6
sridhar281 <sridharyelamanchali025@gmail.com>
2025-12-05 18:22:14 +0530

    .

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -1,5 +1,5 @@
 import { X } from "lucide-react";
-import  useAuthStore  from "../store/UseAuthStore";
+import  useAuthStore  from "../store/useAuthStore";
 import  useChatStore  from "../store/useChatStore";
 
 const ChatHeader = () => {
